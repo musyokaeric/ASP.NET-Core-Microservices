684594d2a05059fa22e173b95d1709ccc56c40e7
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-09-14 19:47:11 +0300

    Catalog.API Data Layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IConfiguration configuration; // Use this to get configuration settings in appsettings.json
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.Seed(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
