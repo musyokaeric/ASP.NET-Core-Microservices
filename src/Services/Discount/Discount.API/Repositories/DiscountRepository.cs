7eb2aac67b16be8b37c69fee1813365fabaa2f8a
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-09-20 13:21:23 +0300

    Discount.API Business Layer - Repository Pattern

---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,65 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Discount.API.Repositories.Interface;
+using Npgsql;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration configuration;
+        NpgsqlConnection connection;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            connection = new NpgsqlConnection(this.configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+            if (coupon == null)
+            {
+                return new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+            }
+            return coupon;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            var created = await connection.ExecuteAsync
+                ("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+                new { Id = coupon.Id, ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+            if (created == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            var updated = await connection.ExecuteAsync
+                ("UPDATE Coupon SET ProductName = @ProductName, Description = @Description, Amount = @Amount WHERE Id = @Id)",
+                new { Id = coupon.Id, ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+            if (updated == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            var deleted = await connection.ExecuteAsync(
+                "DELETE FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+            if (deleted == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
