7528bbad4bbf2c5c63ac3a5d619323a470ad3683
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-10-03 14:51:52 +0300

    Developed Ordering.Infrastructure Layer - Persistence/DbContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+        {
+            if (!context.Orders.Any())
+            {
+                context.Orders.AddRange(GetPreconfiguredOrders());
+                await context.SaveChangesAsync();
+                logger.LogInformation($"Seed database associated with context {typeof(OrderContext).Name}.");
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>()
+            {
+                 new Order() 
+                 {
+                     UserName = "swn", 
+                     FirstName = "Eric", 
+                     LastName = "Musyoka", 
+                     EmailAddress = "musyokaeric@live.com", 
+                     AddressLine = "Nairobi", 
+                     Country = "Kenya", 
+                     TotalPrice = 350 
+                 }
+            };
+        }
+    }
+}
