821e327e15320e31511ddbb313305e9930f4fc57
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-10-06 13:23:49 +0300

    Created RabbitMQ Event in Basket.API as Publisher of BasketCheckoutEvent

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@ using Basket.API.Repositories;
 using Basket.API.Repositories.Interface;
 using Discount.GRPC.GrpcServices;
 using Discount.GRPC.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,16 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
     (options => options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
 
 builder.Services.AddScoped<DisccountGrpcService>();
+
+// Inject MassTransit
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((context, configuration) =>
+    {
+        configuration.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+    });
+});
+// builder.Services.AddMassTransitHostedService(); Not needed in Masstransit v8 and later
 
 var app = builder.Build();
 
